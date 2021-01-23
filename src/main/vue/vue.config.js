179bb330de4297673d813d70766f90f247d77edc
mambo <kdevkr@gmail.com>
2021-01-24 01:16:51 +0900

    Add Production Build Example

--- src/main/vue/vue.config.js
+++ src/main/vue/vue.config.js
@@ -1,7 +1,19 @@
+const path = require('path')
+
 module.exports = {
+  indexPath: '../../templates/index.ftlh',
+  outputDir: path.resolve(__dirname, '../resources/static/dist'),
   devServer: {
     port: 8081,
     proxy: 'http://localhost:8080',
     disableHostCheck: true
+  },
+  chainWebpack: config => {
+    config.plugin('html')
+        .tap(args => {
+          args[0].minify = false
+          args[0].interpolate = true
+          return args
+        })
   }
 };
